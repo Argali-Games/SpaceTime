60160b11dc34086003747cc5f272b478b2a38e39
Alistair-Desktop\Alistair Foggin <alistair.foggin02@gmail.com>
2018-08-10 17:27:24 +0600

    Gravity working for objects

---
+++ Assets/Scripts/ConstantForceController.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ConstantForceController : MonoBehaviour
+{
+	private GameController gameController;
+	private ConstantForce constantForceComponent;
+
+	// Use this for initialization
+	void Start ()
+	{
+		gameController = GameObject.FindGameObjectWithTag("GameController").GetComponent<GameController>();
+		if (!gameController)
+		{
+			Debug.Log("GameController not found");
+			return;
+		}
+
+		constantForceComponent = GetComponent<ConstantForce>();
+		if (!constantForceComponent)
+		{
+			Debug.Log("Constant Force not found");
+			return;
+		}
+
+		UpdateGravity();
+	}
+
+	private void UpdateGravity()
+	{
+		constantForceComponent.force = gameController.GravityDirection * gameController.GravityForce;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (!gameController || !constantForceComponent)
+			return;
+		UpdateGravity();
+	}
+}
---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameController : MonoBehaviour
+{
+	[SerializeField]
+	private Vector3 gravityDirection;
+
+	[SerializeField]
+	private float gravityForce;
+
+	public Vector3 GravityDirection
+	{
+		get
+		{
+			return gravityDirection;
+		}
+		set
+		{
+			gravityDirection = value;
+		}
+	}
+
+	public float GravityForce
+	{
+		get
+		{
+			return gravityForce;
+		}
+		set
+		{
+			gravityForce = value;
+		}
+	}
+
+	// Use this for initialization
+	void Start ()
+	{
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		
+	}
+}
